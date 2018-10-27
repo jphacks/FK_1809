6995985da20b23f54feed0376063e8442b11a2a8
sotaro maehara <s.maehara81@icloud.com>
2018-10-27 15:23:20 +0900

    create recommend system

---
+++ recommend/create_ann_dict.py
@@ -0,0 +1,43 @@
+import cv2
+import glob
+import argparse
+from chainercv import utils
+from chainercv.links import SSD300
+from annoy import AnnoyIndex
+label_name = ("tops", "bottoms")
+model = SSD300(n_fg_class=len(label_name), pretrained_model='via_model')
+
+def crop_img(input_img, query_label="tops"):
+  bboxes, labels, scores = model.predict([input_img])
+  query_index = label_name.index(query_label)
+  if query_index in labels[0]:
+    bbox = [int(i) for i in bboxes[0][0]]
+    croped_img = input_img[:, bbox[0]:bbox[2], bbox[1]:bbox[3]]
+    return croped_img
+
+def main():
+  parser = argparse.ArgumentParser(description='create ann model')
+  parser.add_argument('--imgdir', '-d', type=str, default="",
+                        help='image directry')
+  parser.add_argument('--genre', '-g', type=str, default="tops",
+                        help='genre')
+  args = parser.parse_args()
+  genre = args.genre
+
+  data_dir = args.imgdir
+  if data_dir == "":
+    raise ""
+  data_path = glob.glob("{}/*".format(data_dir))
+  with open("img_list.txt", "w") as f:
+    f.write("\n".join(data_path))
+  annoy_model = AnnoyIndex(256)
+  for i, path in enumerate(data_path):
+    img = utils.read_image(path, color=True)
+    croped_img = crop_img(img, genre)
+    comparing_hist = cv2.calcHist([croped_img], [0], None, [256], [0, 256])
+    annoy_model.add_item(i, comparing_hist[:, 0])
+  annoy_model.build(256)
+  annoy_model.save('{}.ann'.format(genre))
+
+if __name__=='__main__':
+  main()
---
+++ recommend/recommend.py
@@ -0,0 +1,47 @@
+import json
+import cv2
+import argparse
+import numpy as np
+from annoy import AnnoyIndex
+from chainercv import utils
+from chainercv.links import SSD300
+label_name = ("tops", "bottoms")
+model = SSD300(n_fg_class=len(label_name), pretrained_model='via_model')
+
+def crop_img(input_img, query_label="tops"):
+  bboxes, labels, scores = model.predict([input_img])
+  query_index = label_name.index(query_label)
+  if query_index in labels[0]:
+    bbox = [int(i) for i in bboxes[0][0]]
+    croped_img = input_img[:, bbox[0]:bbox[2], bbox[1]:bbox[3]]
+    return croped_img
+
+def main():
+  parser = argparse.ArgumentParser(description='recommend system')
+  parser.add_argument('--query', '-q', type=str, default="",
+                        help='image directry')
+  parser.add_argument('--genre', '-g', type=str, default="tops",
+                        help='genre')
+  args = parser.parse_args()
+  
+  if args.query == "":
+    raise("")
+  genre = args.genre
+  
+  data_path = []
+  with open("img_list.txt", "r") as f:
+    for line in f.readlines():
+      data_path.append(line.rstrip())
+  annoy_model = AnnoyIndex(256)
+  annoy_model.load("{}.ann".format(genre))
+  query_path = args.query
+  query_img = utils.read_image(query_path, color=True)
+  croped_query_img = crop_img(query_img, genre)
+  comparing_hist = cv2.calcHist([croped_query_img], [0], None, [256], [0, 256])
+  predict_indexes = annoy_model.get_nns_by_vector(comparing_hist, 5, search_k=-1)
+  predict_indexes = [data_path[idx] for idx in predict_indexes]
+  with open("recommend_image.json", "w") as f:
+    json.dump(predict_indexes, f)
+          
+if __name__=='__main__':
+  main()
