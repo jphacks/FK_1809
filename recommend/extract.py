caa5c97294a11d88c6457f0b00b160b5121b9e17
Yamato <k3402312@kadai.jp>
2018-10-28 08:21:34 +0900

    extrct bounding box info

--- recommend/extract.py
+++ recommend/extract.py
@@ -16,12 +16,14 @@ dara_dir = "D:/GitHub/FK_1809/wearlog/app/assets/images/wear_images"
 print(dara_dir)
 
 path_list = glob.glob("{}/*".format(dara_dir))
-for path in path_list[:1]:
-    clum = {"img_path": path}
+for path in path_list:
+    print(path)
+    clum = {"img_path": path.split("\\")[-1]}
     img = utils.read_image(path)
     bboxes, labels, scores = model.predict([img])
-    for bbox, label in zip(bbox, label):
-        label_index = label_name.index(label)
-        clum[label_name[label_index]] = bbox
+    for bbox, label in zip(bboxes[0], labels[0]):
+        label_index = label_name[label]
+        clum[label_index] = [int(item) for item in bbox]
+    json_data.append(clum)
 with open("wear_info.json", "w") as f:
-    json.dump(clum, f)
+    json.dump(json_data, f)
