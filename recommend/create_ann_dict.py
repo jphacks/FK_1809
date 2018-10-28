3f06a490aff6a1af61ddb6bbd0509f8e83d905c8
sotaro maehara <s.maehara81@icloud.com>
2018-10-28 14:03:18 +0900

    fix bug

--- recommend/create_ann_dict.py
+++ recommend/create_ann_dict.py
@@ -1,6 +1,8 @@
 import cv2
 import argparse
 import json
+from chainercv import utils
+from annoy import AnnoyIndex
 label_name = ("tops", "bottoms")
 
 def crop_img(input_img, bbox):
@@ -9,28 +11,31 @@ def crop_img(input_img, bbox):
 
 def main():
   parser = argparse.ArgumentParser(description='create ann model')
-  parser.add_argument('--imgdir', '-d', type=str, default="D:/GitHub/FK_1809/wearlog/app/assets/images/wear_images",
+  parser.add_argument('--imgdir', '-d', type=str, default="../wearlog/app/assets/images/wear_images/",
                         help='image directry')
+  parser.add_argument('--bbox', '-b', type=str, default="",
+                        help='bbox image')
   parser.add_argument('--genre', '-g', type=str, default="tops",
                         help='genre')
   args = parser.parse_args()
   genre = args.genre
-
+  
   data_dir = args.imgdir
+  # bbox = [int(item) for item in args.bbox.split(",")]
   if data_dir == "":
-    raise ""
+    raise ValueError("directry is empty")
+
   with open("wear_info.json", "r") as f:
     json_data = json.load(f)
   
-  data_path = glob.glob("{}/*".format(data_dir))
-  with open("img_list.txt", "w") as f:
-    f.write("\n".join(data_path))
   annoy_model = AnnoyIndex(256)
-  for i, path in enumerate(data_path):
-    img = utils.read_image(path, color=True)
-    croped_img = crop_img(img, genre)
+  for idx, item in enumerate(json_data):
+    path = item["img_path"]
+    bbox = item[genre]
+    img = utils.read_image(data_dir+path, color=True)
+    croped_img = crop_img(img, bbox)
     comparing_hist = cv2.calcHist([croped_img], [0], None, [256], [0, 256])
-    annoy_model.add_item(i, comparing_hist[:, 0])
+    annoy_model.add_item(idx, comparing_hist[:, 0])
   annoy_model.build(256)
   annoy_model.save('{}.ann'.format(genre))
 
--- recommend/extract_color.py
+++ recommend/extract_color.py
@@ -11,6 +11,10 @@ cluster.fit(X=cv2_img)
 KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=300,
   n_clusters=5, n_init=10, n_jobs=1, precompute_distances='auto',
   random_state=None, tol=0.0001, verbose=0)
+hist = [0] * 4
+result = cluster.fit_predict(cv2_img)
+for item in result:
+  hist[item] += 1
 print(cluster.cluster_centers_/255)
-plt.bar([1,2,3, 4], [1,1,1,1], color=cluster.cluster_centers_/256)
+plt.bar([1,2,3, 4], hist, color=cluster.cluster_centers_/256)
 plt.show()
